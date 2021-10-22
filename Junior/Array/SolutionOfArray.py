b739ef569d36ebfac5faf155d698e4df91360863
SylarZing <344847204@qq.com>
2021-10-22 10:04:14 +0800

    add TwoSum()

--- Junior/Array/SolutionOfArray.py
+++ Junior/Array/SolutionOfArray.py
@@ -119,12 +119,23 @@ class SolutionOfnumsay:
         print(digits)
     
     # ~~~~~两数之和~~~~~
-    # 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-    # 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+    # 给定一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回它们的数组下标。
+    # 你可以假设每种输入只会对应一个答案。但是数组中同一个元素在答案里不能重复出现。
     # 你可以按任意顺序返回答案。
-    # Hint：
-    def TwoSum(nums = [1,2,3,4,5], target = 6):
-        return
+    # Hint: 暴力破解 遍历就好
+    def TwoSum1(nums = [1,2,3,4,5], target = 6):
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    print([i, j])
+                    return[i ,j]
+    #Hint: 遍历一边nums[i],判断target - nums[i]是否在nums中
+    def TwoSum2(nums = [1,2,3,4,5], target = 6):
+        for i in range(len(nums) - 1, -1, -1):
+            if (target - nums[i] in nums):
+                index = nums.index(target - nums[i])
+                return[i, index]
+
         
     
     # 买卖股票的最佳时机
@@ -150,3 +161,5 @@ class SolutionOfnumsay:
 
 # SolutionOfnumsay.PlusOne()
 
+# SolutionOfnumsay.TwoSum1()
+# SolutionOfnumsay.TwoSum2()
