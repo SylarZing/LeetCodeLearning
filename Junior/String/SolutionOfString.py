0d7665289bd3ddabf2a6bb611ab63a5aac7837bb
SylarZing <344847204@qq.com>
2021-11-04 21:44:08 +0800

    add new file for string solution

---
+++ Junior/String/SolutionOfString.py
@@ -0,0 +1,58 @@
+class SolutionOfString:
+    # ~~~~~翻转字符串~~~~~
+    # 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。
+    # 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+    # Hint: 不使用库函数，收尾交换
+    def ReverseString(s = ["H","a","n","n","a"]):
+        
+        length = len(s)
+        count = (int)(length / 2)
+        
+        for index in range(0, count):
+            tmp = s[index]
+            s[index] = s[length - 1 - index]
+            s[length - 1 - index] = tmp
+        print(s)
+        return
+
+    # ~~~~~整数翻转~~~~~
+    # link: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnx13t/
+    # Hint: 利用求余,反算每一位数字
+    def ReverseInt1(x = -12) -> int:
+        result = 0
+        flag = x > 0
+        if (not flag):
+            x = x * -1
+
+        while(x != 0):
+            a = x % 10
+            x = (int)(x / 10)
+            result = result * 10 + a
+            
+        if (not flag):
+            result = result * -1
+
+        if -2**31 <= result <= 2**31 - 1:
+            return result
+        else:
+            return 0
+    # Hint: 利用字符串转换
+    def ReverseInt2(x = -12) -> int:
+        flag = x > 0
+        if (not flag):
+            x = x * -1
+        strInt = str(x)
+        strInt = strInt[::-1]
+        result = int(strInt)
+        if (not flag):
+            result = result * -1
+        
+        if -2**31 <= result <= 2**31 - 1:
+            return result
+        else:
+            return 0
+
+# SolutionOfString.ReverseString()
+
+# SolutionOfString.ReverseInt1()
+# SolutionOfString.ReverseInt2()
