54cad060330c6aa7aa738c2493aedf9a68410f1a
Zing Cui <zcui@touchpointmed.com>
2021-12-06 10:37:53 +0800

    climbStairs

--- Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
+++ Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
@@ -1,5 +1,10 @@
 class SolutionOfDynamicPlanning:
-    def test():
-        return
+    
+    # ~~~~~爬楼梯~~~~~
+    # Link: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn854d/
+    # Hint
+    def ClimbStairs(N = 5) -> int:
+        return 3
 
 
+SolutionOfDynamicPlanning.ClimbStairs()
