05d90057066de5369c8c1c0f65d553eb49d16fce
Zing Cui <zcui@touchpointmed.com>
2021-12-06 11:23:36 +0800

    Fibonacci sequence

--- Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
+++ Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
@@ -2,9 +2,15 @@ class SolutionOfDynamicPlanning:
     
     # ~~~~~爬楼梯~~~~~
     # Link: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn854d/
-    # Hint
-    def ClimbStairs(N = 5) -> int:
-        return 3
+    # Hint: 斐波那契数列 第n项通项公式
+    def ClimbStairs1(n = 3) -> int:
+        if (n == 1):
+            return 1
+        elif (n == 2):
+            return 2
+        else:
+            return int((((1 + 5**0.5)/2)**(n + 1) - ((1 - 5**0.5)/2)**(n + 1))/(5**0.5))
+        
 
 
-SolutionOfDynamicPlanning.ClimbStairs()
+SolutionOfDynamicPlanning.ClimbStairs1()
