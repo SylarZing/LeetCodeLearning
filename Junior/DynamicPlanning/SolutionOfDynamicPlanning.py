454ff62d642921480e953ca2eb77f3391f3f8bd3
SylarZing <344847204@qq.com>
2021-12-30 11:10:34 +0800

    add MaxSubArray()

--- Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
+++ Junior/DynamicPlanning/SolutionOfDynamicPlanning.py
@@ -44,7 +44,7 @@ class SolutionOfDynamicPlanning:
             maxValue = max(maxValue, prices[index] - minPrice)
         print(maxValue)
         return maxValue
-    # Hint:
+    # Hint: 动态规划
     def MaxProfit2(prices = [2,4,5,1]) -> int:
         # dp[i][0] -- i + 1 天结束时候没股票时的最大收益
         # dp[i][1] -- i + 1 天结束时候有股票时的最大收益
@@ -64,6 +64,28 @@ class SolutionOfDynamicPlanning:
         print(unhold)
         return unhold
 
+    # ~~~~~ 最大子序和 ~~~~~
+    # Link: https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn3cg3/
+    # Hint: 动态规划
+    def MaxSubArray(nums = [-2,1,-3,4,-1,2,1,-5,4]) -> int:
+        # dp[i] -- 表示数组中前i+1（注意这里的i是从0开始的）个元素构成的连续子数组的最大和
+        # dp[i] = max(dp[i - 1], 0) + nums[i]
+        length = len(nums)
+        dp = [0] * length
+        dp[0] = nums[0]
+        maximum = dp[0]
+        for i in range(1, length):
+            if (dp[i - 1] > 0):
+                dp[i] = dp[i - 1] + nums[i]
+            else:
+                dp[i] = nums[i]
+            
+        print(max(dp))
+        return max(dp)
+        
+
+
+
 
 
 
@@ -75,3 +97,5 @@ class SolutionOfDynamicPlanning:
 
 # SolutionOfDynamicPlanning.MaxProfit1()
 # SolutionOfDynamicPlanning.MaxProfit2()
+
+SolutionOfDynamicPlanning.MaxSubArray()
